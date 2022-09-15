d21e55e3c03610f06be58f34c584a126eac930a6
Anthony Bui <anthonyb50@gmail.com>
2022-09-14 23:21:52 -0400

    Added China UnionPay and Switch to code

--- detectNetwork.js
+++ detectNetwork.js
@@ -13,32 +13,76 @@ var detectNetwork = function(cardNumber) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
+  var sixPrefix = cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3] +
+  cardNumber[4] + cardNumber[5];
   var fourPrefix = cardNumber[0] + cardNumber[1] + cardNumber[2] + cardNumber[3];
   var threePrefix = cardNumber[0] + cardNumber[1] + cardNumber[2];
   var twoPrefix = cardNumber[0] + cardNumber[1];
   var onePrefix = cardNumber[0];
   var cardLength = cardNumber.length;
-  var cardPrefixValid = false;
-  var cardLengthValid = false;
+  var cardChinaPrefixValid = false;
+  var cardDiscoverPrefixValid = false;
+  var cardChinaLengthValid = false;
+  var cardMaestroLengthValid = false;
+  var cardSwitchLengthValid = false;
 
-  for (var i = 644; i <= 649; i++) {
+  for (var i = 622126; i <= 622925; i++) {
+    if (sixPrefix === i.toString()) {
+      cardChinaPrefixValid = true;
+    }
+  }
+
+  for (var m = 6282; m <= 6288; j++) {
+    if (fourPrefix === m.toString()) {
+      cardChinaPrefixValid = true;
+    }
+  }
+
+  for (var j = 624; j <= 626; j++) {
+    if (threePrefix === j.toString()) {
+      cardChinaPrefixValid = true;
+    }
+  }
+
+  for (var k = 644; k <= 649; i++) {
     if (threePrefix === i.toString()) {
-      cardPrefixValid = true;
+      cardDiscoverPrefixValid = true;
     }
   }
 
-  for (var j = 12; j <= 19; j++) {
-    if (cardLength === j) {
-      cardLengthValid = true;
+  for (var l = 12; l <= 19; j++) {
+    if (cardLength === l) {
+      cardMaestroLengthValid = true;
     }
   }
 
-  if (fourPrefix === '6011' && (cardLength === 16 || cardLength === 19)) {
+  for (var n = 16; n <= 19; n++) {
+    if (cardLength === n) {
+      cardChinaLengthValid = true;
+    }
+  }
+
+  for (var o = 16; o <= 19; o++) {
+    if (cardLength === 16 || cardLength === 18 || cardLength === 19) {
+      cardSwitchLengthValid = true;
+    } else {
+      continue;
+    }
+  }
+
+  if (cardChinaPrefixValid && cardChinaLengthValid) {
+    return 'China UnionPay';
+  } else if ((sixPrefix === '564182' || sixPrefix === '633110') && cardSwitchLengthValid) {
+    return 'Switch';
+  } else if ((fourPrefix === '4903' || fourPrefix === '4905' || fourPrefix === '4911' ||
+    fourPrefix === '6333' || fourPrefix === '6759') && cardSwitchLengthValid) {
+    return 'Switch';
+  } else if (fourPrefix === '6011' && (cardLength === 16 || cardLength === 19)) {
     return 'Discover';
-  } else if (cardLengthValid && (fourPrefix === '5018' || fourPrefix === '5020' ||
+  } else if (cardMaestroLengthValid && (fourPrefix === '5018' || fourPrefix === '5020' ||
     fourPrefix === '5038' || fourPrefix === '6304')) {
     return 'Maestro';
-  } else if (cardPrefixValid && (cardLength === 16 || cardLength === 19)) {
+  } else if (cardDiscoverPrefixValid && (cardLength === 16 || cardLength === 19)) {
     return 'Discover';
   } else if ((twoPrefix === '38' || twoPrefix === '39') && cardLength === 14) {
     return 'Diner\'s Club';
